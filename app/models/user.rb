be7a1f64829362345f919b443ef43defc822d923
estherkang14 <estherkang14@gmail.com>
2020-09-09 15:59:54 -0400

    fixed something in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }, presence: true
-    validates :password, presence: true, uniqueness: {case_sensitive: true}, length: { minimum: 6 }
+    # validates :password, presence: true, uniqueness: {case_sensitive: true}, length: { minimum: 6 }
     validates :zipcode, presence: true
 
     has_many :events
